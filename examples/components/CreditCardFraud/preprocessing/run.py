c21d5c8f206f998e2dfa9da6cc47932bbe4bca15
David Majercak <damajercak@microsoft.com>
2022-11-10 19:25:31 -0800

    training

--- examples/components/CreditCardFraud/preprocessing/run.py
+++ examples/components/CreditCardFraud/preprocessing/run.py
@@ -2,14 +2,17 @@ import os
 import argparse
 import logging
 import sys
+import json
 
-from torchvision import transforms
-from torchvision.utils import save_image
-from torch.utils.data import Dataset
-import torch
+from sklearn.preprocessing import OneHotEncoder
 import pandas as pd
 import mlflow
+import hydra
 
+from azureml.core import Run, Workspace
+
+ENCODERS = {}
+STATES_REGIONS = {}
 
 def get_arg_parser(parser=None):
     """Parse the command line arguments for merge using argparse.
@@ -32,47 +35,70 @@ def get_arg_parser(parser=None):
     parser.add_argument("--raw_testing_data", type=str, required=True, help="")
     parser.add_argument("--train_output", type=str, required=True, help="")
     parser.add_argument("--test_output", type=str, required=True, help="")
-    parser.add_argument("--config", type=str, required=False)
     parser.add_argument(
         "--metrics_prefix", type=str, required=False, help="Metrics prefix"
     )
     return parser
 
 
-class FraudDataset(Dataset):
-    """FraudDataset Dataset - combination of features and labels
-
-    Args:
-        feature: Transaction detail tensors
-        target: Tensor of labels corresponding to features
-
-    Returns:
-        None
-    """
-
-    def __init__(self, feature, target=None):
-        self.X = feature
-        self.Y = target
-
-    def __len__(self):
-        return len(self.X)
-
-    def __getitem__(self, idx):
-        if self.Y is None:
-            return [self.X[idx]]
-        return self.X[idx], self.Y[idx]
-
 def apply_transforms(df):
-    df["full_name"] = df["first"] + " " + df["last"]
-    df['region'] = df['state'].map(STATES_REGIONS)
+    global ENCODERS
+
+    useful_props = [
+        "amt",
+        "cc_num",
+        "merch_lat",
+        "merch_long",
+        "category",
+        "region",
+        "gender",
+        "state",
+        "zip",
+        "lat",
+        "long",
+        "city_pop",
+        "job",
+        "dob",
+        "trans_date_trans_time",
+        "is_fraud",
+    ]
+    categorical = ["category", "region", "gender", "state", "job"]
+    datetimes = ["dob", "trans_date_trans_time"]
+    normalize = ["dob", "age"]
+
+    # Filter only useful columns
+    df = df[useful_props]
+
+    df.loc[:, "age"] = (pd.Timestamp.now() - pd.to_datetime(df["dob"])) // pd.Timedelta('1y')
+    for column in categorical:
+        if column not in ENCODERS:
+            print(f"Creating encoder for column: {column}")
+            # Simply set all zeros if the category is unseen
+            encoder = OneHotEncoder(handle_unknown="ignore")
+            encoder.fit(df[column].values.reshape(-1,1))
+            ENCODERS[column] = encoder
+
+        encoder = ENCODERS.get(column)
+        encoded_data = encoder.transform(df[column].values.reshape(-1,1)).toarray()
+        encoded_df = pd.DataFrame(encoded_data, columns = [column + "_" + "_".join(x.split("_")[1:]) for x in encoder.get_feature_names()])
+        encoded_df.index = df.index
+        df = df.join(encoded_df).drop(column, axis=1)
+
+    for column in datetimes:
+        df.loc[:, column] = pd.to_datetime(df[column]).view("int64")
+    for column in normalize:
+        df.loc[:, column] = (df[column] - df[column].min())/(df[column].max() - df[column].min())
+
+    return df
+
 
 def preprocess_data(
     raw_training_data,
     raw_testing_data,
     train_data_dir="./",
     test_data_dir="./",
-    config=None,
     metrics_prefix="default-prefix",
+    config=None,
 ):
     """Preprocess the raw_training_data and raw_testing_data and save the processed data to train_data_dir and test_data_dir.
 
@@ -88,53 +114,41 @@ def preprocess_data(
     logger.info(
         f"Raw Training Data path: {raw_training_data}, Raw Testing Data path: {raw_testing_data}, Processed Training Data dir path: {train_data_dir}, Processed Testing Data dir path: {test_data_dir}"
     )
+
+    logger.debug("Loading regions from \"us_regions.csv\"")
+    global STATES_REGIONS
+    df_states_regions = pd.read_csv(hydra.utils.get_original_cwd() + "/us_regions.csv")
+    STATES_REGIONS = {
+        row.StateCode: row.Region for row in df_states_regions.itertuples()
+    }
+    REGIONS = list(df_states_regions["Region"].unique())
+
+
+    logger.debug(f"Loading data...")
     train_data = pd.read_csv(raw_training_data)
     test_data = pd.read_csv(raw_testing_data)
 
-    train_data["Region"] = train_data["State"].map(STATES_REGIONS)
-    test_data["Region"] = test_data["State"].map(STATES_REGIONS)
+    logger.debug(f"Filtering regions...")
+    train_data.loc[:, "region"] = train_data["state"].map(STATES_REGIONS)
+    train_data = train_data[train_data["region"].str.match(config.region)]
+    test_data.loc[:, "region"] = test_data["state"].map(STATES_REGIONS)
+    test_data = test_data[test_data["region"].str.match(config.region)]
 
-    logger.debug(f"Segregating labels and features")
-    X_train = torch.tensor(train_data.loc[:, train_data.columns != "is_fraud"].values)
-    y_train = torch.tensor(train_data["is_fraud"].values)
-
-    X_test = torch.tensor(test_data.loc[:, test_data.columns != "is_fraud"].values)
-    y_test = torch.tensor(test_data["is_fraud"].values)
-
-    train_set = FraudDataset(X_train.float(), y_train)
-    train_loader = torch.utils.data.DataLoader(train_set, batch_size=128, shuffle=True)
-    data = next(iter(train_loader))
-    mean = data[0].mean()
-    std = data[0].std()
-
-    transform = transforms.Compose(
-        [
-            transforms.ToPILImage(),
-            transforms.RandomAffine(degrees=30),
-            transforms.RandomPerspective(),
-            transforms.ToTensor(),
-            transforms.Normalize(mean, std),
-        ]
-    )
+    logger.debug(f"Applying transformations...")
+    train_data = apply_transforms(train_data)
+    test_data = apply_transforms(test_data)
 
     logger.info(
-        f"Transforming images data and saving processed data to {train_data_dir} and {test_data_dir}"
+        f"Saving processed data to {train_data_dir} and {test_data_dir}"
     )
-    train_dataset = MnistDataset(X_train.float(), y_train, transform=transform)
-    test_dataset = MnistDataset(X_test.float(), y_test, transform=transform)
-    datasets = {"train": train_dataset, "test": test_dataset}
+    train_data.to_csv(train_data_dir + "/data.csv")
+    test_data.to_csv(test_data_dir + "/data.csv")
 
     # Mlflow logging
-    log_metadata(X_train, X_test, metrics_prefix)
-
-    for x in ["train", "test"]:
-        processed_data_dir = train_data_dir if x == "train" else test_data_dir
-        for idx, (data, target) in enumerate(datasets[x]):
-            os.makedirs(processed_data_dir + f"/{target}", exist_ok=True)
-            save_image(data, processed_data_dir + f"/{target}/{idx}.jpg")
+    log_metadata(train_data, test_data, metrics_prefix)
 
 
-def log_metadata(X_train, X_test, metrics_prefix):
+def log_metadata(train_df, test_df, metrics_prefix):
     with mlflow.start_run() as mlflow_run:
         # get Mlflow client
         mlflow_client = mlflow.tracking.client.MlflowClient()
@@ -143,37 +157,14 @@ def log_metadata(X_train, X_test, metrics_prefix):
         mlflow_client.log_metric(
             run_id=root_run_id,
             key=f"{metrics_prefix}/Number of train datapoints",
-            value=f"{X_train.size(dim=0)}",
+            value=f"{train_df.shape[0]}",
         )
 
         mlflow_client.log_metric(
             run_id=root_run_id,
             key=f"{metrics_prefix}/Number of test datapoints",
-            value=f"{X_test.size(dim=0)}",
+            value=f"{test_df.shape[0]}",
         )
-
-
-def run(args):
-    """Run script with arguments (the core of the component).
-
-    Args:
-        args (argparse.namespace): command line arguments provided to script
-    """
-
-    global STATES_REGIONS
-
-    df_states_regions = pd.read_csv("./us_regions.csv")
-    STATES_REGIONS = {row.StateCode: row.Region for row in df_states_regions.itertuples()}
-    REGIONS = list(df_states_regions['Region'].unique())
-
-    preprocess_data(
-        args.raw_training_data,
-        args.raw_testing_data,
-        args.config,
-        args.train_output,
-        args.test_output,
-        args.metrics_prefix,
-    )
 
 
 def main(cli_args=None):
@@ -186,12 +177,35 @@ def main(cli_args=None):
     """
     # build an arg parser
     parser = get_arg_parser()
-
     # run the parser on cli args
     args = parser.parse_args(cli_args)
-
     print(f"Running script with arguments: {args}")
-    run(args)
+
+    # Get runtime specific configuration
+    run: Run = Run.get_context()
+    compute_target = run.get_details()['target']
+    print(f"Compute target: {compute_target}")
+    sys.argv = [f"--config_name={compute_target}"]
+
+
+    @hydra.main(config_path="config", config_name="default")
+    def run(runtime_config):
+        """Run script with arguments (the core of the component).
+
+        Args:
+            args (argparse.namespace): command line arguments provided to script
+        """
+
+        preprocess_data(
+            args.raw_training_data,
+            args.raw_testing_data,
+            args.train_output,
+            args.test_output,
+            args.metrics_prefix,
+            config=runtime_config,
+        )
+
+    run()
 
 
 if __name__ == "__main__":
