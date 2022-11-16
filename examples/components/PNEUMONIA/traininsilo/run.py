455df80f71a189148067c5066e677f106388be55
thomasp-ms <XXX@me.com>
2022-11-16 12:24:34 -0800

    cite sources, remove wrongly added licenses

--- examples/components/PNEUMONIA/traininsilo/run.py
+++ examples/components/PNEUMONIA/traininsilo/run.py
@@ -1,17 +1,4 @@
-# Copyright (c) 2021-2022, NVIDIA CORPORATION.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
+# This file defining the learner was adapted from https://github.com/Azure/medical-imaging/blob/main/federated-learning/pneumonia-federated/custom/pt_learner.py to run directly on Azure ML without using NVFlare.
 """Script for training component."""
 import argparse
 import logging
