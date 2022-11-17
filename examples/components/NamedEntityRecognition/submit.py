d99a3645dcf7b7f907fc4cc70d72152a0db4c2b4
unknown <Mitgarg17495@gmail.com>
2022-11-16 17:12:38 -0800

    add readme

--- examples/components/NamedEntityRecognition/submit.py
+++ examples/components/NamedEntityRecognition/submit.py
@@ -51,7 +51,7 @@ parser.add_argument(
 parser.add_argument(
     "--example",
     required=False,
-    choices=["MNIST", "HELLOWORLD", "PrivateEntityRecognition"],
+    choices=["MNIST", "HELLOWORLD", "NamedEntityRecognition"],
     default="MNIST",
     help="example name",
 )
