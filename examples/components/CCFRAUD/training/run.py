8c212ccc10a387d0986f5f1f34dce45705679dae
David Majercak <damajercak@microsoft.com>
2022-11-17 18:36:38 -0800

    successful training through lstm

--- examples/components/CCFRAUD/training/run.py
+++ examples/components/CCFRAUD/training/run.py
@@ -2,10 +2,12 @@
 import argparse
 import logging
 import sys
+import copy
 
 import mlflow
 import torch
 import pandas as pd
+import numpy as np
 from torch import nn
 from torchmetrics.functional import precision_recall, accuracy
 from torch.optim import Adam
@@ -14,70 +16,62 @@ from torch.utils.data import Dataset
 from mlflow import log_metric, log_param
 from typing import List
 import models as models
+import datasets as datasets
 
 
 class RunningMetrics:
     def __init__(self, metrics: List[str], prefix: str = None) -> None:
-        self._running_metrics = {name: 0 for name in metrics}
-        self._batch_count = 0
+        self._allowed_metrics = copy.deepcopy(metrics)
+        self._running_step_metrics = {name: 0 for name in metrics}
+        self._running_global_metrics = {name: 0 for name in metrics}
+        self._batch_count_step = 0
+        self._batch_count_global = 0
         self._prefix = "" if prefix is None else prefix
 
     def add_metric(self, name: str, value: float):
-        if name not in self._running_metrics:
+        if name not in self._allowed_metrics:
             raise ValueError(f"Metric with name '{name}' not in logged metrics")
-        self._running_metrics[name] += value
+        self._running_step_metrics[name] += value
+        self._running_global_metrics[name] += value
 
     def step(self):
-        self._batch_count += 1
+        self._batch_count_step += 1
+        self._batch_count_global += 1
 
-    def reset(self):
-        self._batch_count = 0
-        self._running_metrics = {name: 0 for name in self._running_metrics}
+    def reset_step(self):
+        self._batch_count_step = 0
+        self._running_step_metrics = {name: 0 for name in self._running_step_metrics}
 
-    def get_all(self):
-        return {
-            f"{self._prefix}_{name}": value / self._batch_count
-            for name, value in self._running_metrics.items()
+    def reset_global(self):
+        self.reset_step()
+        self._batch_count_global = 0
+        self._running_global_metrics = {
+            name: 0 for name in self._running_global_metrics
         }
 
+    def get_step(self):
+        return {
+            f"{self._prefix}_{name}": value / self._batch_count_step
+            for name, value in self._running_step_metrics.items()
+        }
 
-class FraudDataset(Dataset):
-    """FraudDataset Dataset - combination of features and labels
-
-    Args:
-        feature: Transaction detail tensors
-        target: Tensor of labels corresponding to features
-
-    Returns:
-        None
-    """
-
-    def __init__(self, df):
-        self.X = torch.tensor(
-            df.loc[:, df.columns != "is_fraud"].values, dtype=torch.float
-        )
-        self.Y = torch.tensor(df.loc[:, "is_fraud"].values, dtype=torch.int)
-
-    def __len__(self):
-        return len(self.X)
-
-    def __getitem__(self, idx):
-        if self.Y is None:
-            return [self.X[idx]]
-        return self.X[idx], self.Y[idx]
+    def get_global(self):
+        return {
+            f"{self._prefix}_{name}": value / self._batch_count_global
+            for name, value in self._running_global_metrics.items()
+        }
 
 
 class CCFraudTrainer:
     def __init__(
         self,
         model_name,
-        input_dim=576,
         train_data_dir="./",
         test_data_dir="./",
         model_path=None,
         lr=0.01,
         epochs=1,
-        batch_size=10000,
+        batch_size=10,
         experiment_name="default-experiment",
         iteration_name="default-iteration",
     ):
@@ -102,7 +96,6 @@ class CCFraudTrainer:
 
         # Training setup
         self._lr = lr
-        self._input_dim = input_dim
         self._epochs = epochs
         self._batch_size = batch_size
         self._experiment_name = experiment_name
@@ -110,8 +103,8 @@ class CCFraudTrainer:
 
         self.device_ = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-        self.train_dataset_, self.test_dataset_ = self.load_dataset(
-            train_data_dir, test_data_dir
+        self.train_dataset_, self.test_dataset_, self._input_dim = self.load_dataset(
+            train_data_dir, test_data_dir, model_name
         )
         self.train_loader_ = DataLoader(
             self.train_dataset_, batch_size=batch_size, shuffle=True
@@ -121,15 +114,13 @@ class CCFraudTrainer:
         )
 
         # Build model
-        self.model_ = getattr(models, model_name)(self.train_dataset_.X.shape[1]).to(
-            self.device_
-        )
+        self.model_ = getattr(models, model_name)(self._input_dim).to(self.device_)
         self._model_path = model_path
 
         self.criterion_ = nn.BCELoss()
-        self.optimizer_ = Adam(self.model_.parameters(), lr=self._lr)
+        self.optimizer_ = Adam(self.model_.parameters(), lr=self._lr, weight_decay=1e-5)
 
-    def load_dataset(self, train_data_dir, test_data_dir):
+    def load_dataset(self, train_data_dir, test_data_dir, model_name):
         """Load dataset from {train_data_dir} and {test_data_dir}
 
         Args:
@@ -137,21 +128,21 @@ class CCFraudTrainer:
             test_data_dir(str, optional): Testing data directory path
         """
         logger.info(f"Train data dir: {train_data_dir}, Test data dir: {test_data_dir}")
+        self.fraud_weight_ = np.loadtxt(train_data_dir + "/fraud_weight.txt").item()
         train_df = pd.read_csv(train_data_dir + "/data.csv")
         test_df = pd.read_csv(test_data_dir + "/data.csv")
-        train_dataset = FraudDataset(train_df)
-        test_dataset = FraudDataset(test_df)
+        if model_name == "SimpleLinear":
+            train_dataset = datasets.FraudDataset(train_df)
+            test_dataset = datasets.FraudDataset(test_df)
+        else:
+            train_dataset = datasets.FraudTimeDataset(train_df)
+            test_dataset = datasets.FraudTimeDataset(test_df)
 
         logger.info(
             f"Train data samples: {len(train_df)}, Test data samples: {len(test_df)}"
         )
 
-        self.fraud_weight_ = (
-            train_df["is_fraud"].value_counts()[0]
-            / train_df["is_fraud"].value_counts()[1]
-        )
-
-        return train_dataset, test_dataset
+        return train_dataset, test_dataset, train_df.shape[1] - 1
 
     def log_params(self, client, run_id):
         client.log_param(
@@ -202,7 +193,7 @@ class CCFraudTrainer:
             self.model_.load_state_dict(torch.load(checkpoint + "/model.pt"))
 
         with mlflow.start_run() as mlflow_run:
-            num_of_batches_before_logging = 10
+            num_of_batches_before_logging = 50
 
             # get Mlflow client and root run id
             mlflow_client = mlflow.tracking.client.MlflowClient()
@@ -220,7 +211,7 @@ class CCFraudTrainer:
             test_metrics = RunningMetrics(
                 ["accuracy", "precision", "recall"], prefix="test"
             )
-            for epoch in range(self._epochs):
+            for epoch in range(1, self._epochs + 1):
                 self.model_.train()
 
                 for i, batch in enumerate(self.train_loader_):
@@ -228,14 +219,15 @@ class CCFraudTrainer:
                     # Zero gradients for every batch
                     self.optimizer_.zero_grad()
 
-                    predictions = self.model_(data)
+                    predictions, net_loss = self.model_(data)
                     self.criterion_.weight = (
                         ((labels == 1) * (self.fraud_weight_ - 1)) + 1
                     ).to(self.device_)
                     # Compute loss
-                    loss = self.criterion_(
-                        predictions.reshape(-1), labels.type(torch.float)
-                    )
+                    loss = self.criterion_(predictions, labels.type(torch.float))
+                    if net_loss is not None:
+                        # Make the criterion magnitude more important than possible network loss
+                        loss += net_loss * 1e-5
 
                     # Compute gradients and adjust learning weights
                     loss.backward()
@@ -257,13 +249,15 @@ class CCFraudTrainer:
                     )
                     train_metrics.step()
 
-                    if (i + 1) != 0 and (i + 1) % num_of_batches_before_logging == 0:
+                    if (i + 1) % num_of_batches_before_logging == 0 or (i + 1) == len(
+                        self.train_loader_
+                    ):
                         log_message = [
                             f"Epoch: {epoch}/{self._epochs}",
                             f"Iteration: {i+1}/{len(self.train_loader_)}",
                         ]
 
-                        for name, value in train_metrics.get_all().items():
+                        for name, value in train_metrics.get_step().items():
                             log_message.append(f"{name}: {value}")
                             self.log_metrics(
                                 mlflow_client,
@@ -272,6 +266,7 @@ class CCFraudTrainer:
                                 value,
                             )
                         logger.info(", ".join(log_message))
+                        train_metrics.reset_step()
 
                 test_metrics = self.test()
 
@@ -280,7 +275,7 @@ class CCFraudTrainer:
                 ]
 
                 # log test metrics after each epoch
-                for name, value in test_metrics.get_all().items():
+                for name, value in test_metrics.get_global().items():
                     log_message.append(f"{name}: {value}")
                     self.log_metrics(
                         mlflow_client,
@@ -290,18 +285,18 @@ class CCFraudTrainer:
                     )
                 logger.info(", ".join(log_message))
 
-            # log metrics at the pipeline level
-            for name, value in train_metrics.get_all().items():
-                log_message.append(f"{name}: {value}")
-                self.log_metrics(
-                    mlflow_client,
-                    root_run_id,
-                    name,
-                    value,
-                    pipeline_level=True,
-                )
+                # log metrics at the pipeline level
+                for name, value in train_metrics.get_global().items():
+                    log_message.append(f"{name}: {value}")
+                    self.log_metrics(
+                        mlflow_client,
+                        root_run_id,
+                        name,
+                        value,
+                        pipeline_level=True,
+                    )
 
-            train_metrics.reset()
+                train_metrics.reset_global()
 
     def test(self):
         """Test the trained model and report test loss and accuracy"""
@@ -313,14 +308,15 @@ class CCFraudTrainer:
         with torch.no_grad():
             for data, target in self.test_loader_:
                 data, labels = data.to(self.device_), target.to(self.device_)
-                predictions = self.model_(data)
+                predictions, net_loss = self.model_(data)
 
                 self.criterion_.weight = (
                     ((labels == 1) * (self.fraud_weight_ - 1)) + 1
                 ).to(self.device_)
-                loss = self.criterion_(
-                    predictions.reshape(-1), labels.type(torch.float)
-                ).item()
+                loss = self.criterion_(predictions, labels.type(torch.float)).item()
+                if net_loss is not None:
+                    # Make the criterion magnitude more important than possible network loss
+                    loss = loss * 10 + net_loss
 
                 precision, recall = precision_recall(
                     preds=predictions.detach(), target=labels
@@ -406,6 +402,7 @@ def run(args):
         model_path=args.model_path + "/model.pt",
         lr=args.lr,
         epochs=args.epochs,
+        batch_size=args.batch_size,
         experiment_name=args.metrics_prefix,
         iteration_name=args.iteration_name,
     )
