2b2392dd6ddfb597aa64e77bee32bcef866cdfb5
David Majercak <damajercak@microsoft.com>
2022-11-23 10:36:07 -0800

    upload data script

--- examples/components/CCFRAUD/upload_data/run.py
+++ examples/components/CCFRAUD/upload_data/run.py
@@ -10,6 +10,7 @@ import azure.ai.ml.identity as identity
 
 from zipfile import ZipFile
 from azureml.core import Run, Workspace
+from azureml.core.keyvault import Keyvault
 from azure.ai.ml import MLClient
 from azure.ai.ml.entities import Data
 from azure.ai.ml.constants import AssetTypes, InputOutputModes
@@ -24,30 +25,35 @@ SPLITS = {
     4: [["South"], ["West"], ["Midwest"], ["Northeast"]],
 }
 
+
 def get_azure_credential() -> AzureMLOnBehalfOfCredential:
-    client_id = os.environ.get('DEFAULT_IDENTITY_CLIENT_ID')
-    credential = ManagedIdentityCredential(client_id=client_id)
-    return credential #AzureMLOnBehalfOfCredential()
+    return AzureMLOnBehalfOfCredential()
 
-def get_kaggle_client(credential):
-    secret_client = SecretClient(vault_url="https://kv-aml-fldemodm0.vault.azure.net/", credential=credential)
-    
-    print(secret_client.get_secret("kaggleusername").value)
-    print(secret_client.get_secret("kagglekey").value)
 
-    os.environ['KAGGLE_USERNAME'] = secret_client.get_secret("kaggleusername").value
-    os.environ['KAGGLE_KEY'] = secret_client.get_secret("kagglekey").value
+def get_kaggle_client(kv: Keyvault):
+
+    os.environ["KAGGLE_USERNAME"] = kv.get_secret("kaggleusername")
+    os.environ["KAGGLE_KEY"] = kv.get_secret("kagglekey")
 
     from kaggle.api.kaggle_api_extended import KaggleApi
+
     api = KaggleApi()
     api.authenticate()
     return api
 
-def get_ml_client(credential) -> MLClient:
+
+def get_workspace() -> Workspace:
     run: Run = Run.get_context()
     logging.info(f"Got run context: {run}")
     workspace: Workspace = run.experiment.workspace
+    return workspace
 
+
+def get_key_vault(workspace) -> Keyvault:
+    return workspace.get_default_keyvault()
+
+
+def get_ml_client(credential, workspace) -> MLClient:
     subscription_id = workspace.subscription_id
     resource_group = workspace.resource_group
     workspace_name = workspace.name
@@ -60,8 +66,10 @@ def get_ml_client(credential) -> MLClient:
     )
     return ml_client
 
+
 def download_kaggle_dataset(kaggle_client, path):
     kaggle_client.dataset_download_files("kartik2112/fraud-detection", path=path)
+
 
 def run(args):
     """Run script with arguments (the core of the component).
@@ -70,56 +78,47 @@ def run(args):
         args (argparse.namespace): command line arguments provided to script
     """
 
-    if args.split_count < 1 or args.split_count > 4:
-        raise Exception("Number of splits must be between 1 and 4 (included)!")
+    split_count = sum(
+        [0 if getattr(args, f"silo_{i}_data", None) is None else 1 for i in range(1, 5)]
+    )
 
-    credential = get_azure_credential()
-    kaggle_client = get_kaggle_client(credential)
+    if split_count < 1 or split_count > 4:
+        raise Exception("Number of splits/silos must be between 1 and 4 (included)!")
+
+    ws = get_workspace()
+    kv = get_key_vault(ws)
+    kaggle_client = get_kaggle_client(kv)
     download_kaggle_dataset(kaggle_client, "./dataset")
 
     with ZipFile("./dataset/fraud-detection.zip", "r") as zObject:
-        zObject.extractall('./dataset/extracted')
+        zObject.extractall("./dataset/extracted")
 
     df_train = pd.read_csv("./dataset/extracted/fraudTrain.csv")
+    print(f"Loaded train dataset with {len(df_train)} rows")
     df_test = pd.read_csv("./dataset/extracted/fraudTrain.csv")
+    print(f"Loaded test dataset with {len(df_train)} rows")
     regions_df = pd.read_csv("./us_regions.csv")
     state_region = {row.StateCode: row.Region for row in regions_df.itertuples()}
+    print(f"Loaded state/regions:\n {state_region}")
 
     df_train.loc[:, "region"] = df_train["state"].map(state_region)
     df_test.loc[:, "region"] = df_test["state"].map(state_region)
 
-    ml_client = get_ml_client(credential)
     os.makedirs("./dataset/filtered/")
-    for i, regions in enumerate(SPLITS[args.split_count]):
-        train_name = f"cc_fraud_train_split_{i}"
-        test_name = f"cc_fraud_test_split_{i}"
-        train_path = f"./dataset/filtered/{train_name}.csv"
-        test_path = f"./dataset/filtered/{test_name}.csv"
+    for i, regions in enumerate(SPLITS[split_count]):
 
-        df_train_filtered = df_train[df_train["state"].isin(regions)]
-        df_test_filtered = df_test[df_test["state"].isin(regions)]
+        print(f"Filtering regions: {regions}")
+        data_path = getattr(args, f"silo_{i + 1}_data", None)
+        train_path = f"{data_path}/train.csv"
+        test_path = f"{data_path}/test.csv"
+
+        df_train_filtered = df_train[df_train["region"].isin(regions)]
+        df_test_filtered = df_test[df_test["region"].isin(regions)]
+        print(f"Filtered train dataset has {len(df_train_filtered)} rows")
+        print(f"Filtered test dataset has {len(df_test_filtered)} rows")
 
         df_train_filtered.to_csv(train_path, index=False)
         df_test_filtered.to_csv(test_path, index=False)
-
-        ds_name = args.target_ds.split(",")[i]
-        datastore = ml_client.datastores.get(ds_name)
-        # target_path = f"azureml://datastores/{ds_name}/paths/cc_fraud/{train_name}"
-        train_dataset = Data(
-            path=train_path,
-            type=AssetTypes.URI_FILE,
-            name=train_name,
-            datastore=datastore
-        )
-        ml_client.data.create_or_update(train_dataset)
-        test_dataset = Data(
-            path=test_path,
-            type=AssetTypes.URI_FILE,
-            name=test_name,
-            datastore=datastore
-        )
-        ml_client.data.create_or_update(test_dataset)
-
 
 
 def get_arg_parser(parser=None):
@@ -139,9 +138,35 @@ def get_arg_parser(parser=None):
     if parser is None:
         parser = argparse.ArgumentParser(description=__doc__)
 
-    parser.add_argument("--split_count", default=3, type=int, required=False, help="Batch Size")
-    parser.add_argument("--target_ds", type=str, required=True, help="Comma separated names of storage accounts to use for datasets")
+    parser.add_argument(
+        "--silo_1_data",
+        type=str,
+        required=True,
+        help="Output folder for silo 1",
+    )
+    parser.add_argument(
+        "--silo_2_data",
+        type=str,
+        required=False,
+        default=None,
+        help="Output folder for silo 2",
+    )
+    parser.add_argument(
+        "--silo_3_data",
+        type=str,
+        required=False,
+        default=None,
+        help="Output folder for silo 3",
+    )
+    parser.add_argument(
+        "--silo_4_data",
+        type=str,
+        required=False,
+        default=None,
+        help="Output folder for silo 4",
+    )
     return parser
+
 
 def main(cli_args=None):
     """Component main function.
