792b9c1150ce00a0f2cbaead21e1c1246aac58d6
unknown <Mitgarg17495@gmail.com>
2022-11-28 16:38:33 -0800

    change data uploading approach

--- examples/components/NER/upload_data/run.py
+++ examples/components/NER/upload_data/run.py
@@ -1,42 +1,10 @@
+"""Script for mock components."""
 import argparse
 import logging
 import sys
+import os
 
 from datasets import load_dataset
-
-
-def get_arg_parser(parser=None):
-    """Parse the command line arguments for merge using argparse.
-
-    Args:
-        parser (argparse.ArgumentParser or CompliantArgumentParser):
-        an argument parser instance
-
-    Returns:
-        ArgumentParser: the argument parser instance
-
-    Notes:
-        if parser is None, creates a new parser instance
-    """
-    # add arguments that are specific to the component
-    if parser is None:
-        parser = argparse.ArgumentParser(description=__doc__)
-
-    parser.add_argument(
-        "--train_data_paths",
-        type=str,
-        nargs="+",
-        required=True,
-        help="Output train data paths",
-    )
-    parser.add_argument(
-        "--test_data_paths",
-        type=str,
-        nargs="+",
-        required=True,
-        help="Output test data paths",
-    )
-    return parser
 
 
 def run(args):
@@ -46,37 +14,68 @@ def run(args):
         args (argparse.namespace): command line arguments provided to script
     """
 
-    # validation
-    assert len(args.train_data_paths) == len(
-        args.test_data_paths
-    ), "number of train and test data paths must be equal"
-
     df = load_dataset("tner/multinerd", "en", split="test")
     df = df.shuffle(seed=42)
 
-    total_num_of_silos = len(args.train_data_paths)
+    # partititon dataset
+    df_partition = df.shard(num_shards=args.silo_count, index=args.silo_index)
 
-    for idx in range(total_num_of_silos):
+    # train test split
+    df_dict = df_partition.train_test_split(test_size=0.1)
 
-        # partititon dataset
-        df_partition = df.shard(num_shards=total_num_of_silos, index=idx)
+    # save data
+    df_dict["train"].save_to_disk(args.raw_train_data)
+    df_dict["test"].save_to_disk(args.raw_test_data)
 
-        # train test split
-        df_dict = df_partition.train_test_split(test_size=0.1)
 
-        # save data
-        df_dict["train"].save_to_disk(args.train_data_paths[idx])
-        df_dict["test"].save_to_disk(args.test_data_paths[idx])
+def get_arg_parser(parser=None):
+    """Parse the command line arguments for merge using argparse.
+    Args:
+        parser (argparse.ArgumentParser or CompliantArgumentParser):
+        an argument parser instance
+    Returns:
+        ArgumentParser: the argument parser instance
+    Notes:
+        if parser is None, creates a new parser instance
+    """
+    # add arguments that are specific to the component
+    if parser is None:
+        parser = argparse.ArgumentParser(description=__doc__)
+
+    parser.add_argument(
+        "--silo_count",
+        type=int,
+        required=True,
+        help="Number of silos",
+    )
+    parser.add_argument(
+        "--silo_index",
+        type=int,
+        required=True,
+        help="Index of the current silo",
+    )
+    parser.add_argument(
+        "--raw_train_data",
+        type=str,
+        required=True,
+        help="Output folder for train data",
+    )
+    parser.add_argument(
+        "--raw_test_data",
+        type=str,
+        required=True,
+        help="Output folder for test data",
+    )
+    return parser
 
 
 def main(cli_args=None):
     """Component main function.
-
     It parses arguments and executes run() with the right arguments.
-
     Args:
         cli_args (List[str], optional): list of args to feed script, useful for debugging. Defaults to None.
     """
+
     # build an arg parser
     parser = get_arg_parser()
 
