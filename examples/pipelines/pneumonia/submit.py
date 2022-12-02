9e1c04fa92fd764b44e5ed81657a5edfd7a5df18
Jeff Omhover <jf.omhover@gmail.com>
2022-12-02 14:01:23 -0800

    Lots of micro-fixes after bug bashing all 3 industry examples (#194)
    related to components:
    * create distinct names for all components of each scenario
    * polish component descriptions
    * remove unused mnist datatransfer and postprocessing components
    * upgrade all MCR images to a more recent OS
    * cut some unnecessary dependencies
    * use curated environments whenever possible (to speed up job build time)
    
    related to pipelines:
    * fix issues with ccfraud submit script (path to shared folder)
    * remove unnecessary json+azure imports in submit scripts
    * align all 3 submissions scripts
    * in upload data pipeline, make --example required without default value to force intentional decision
    * in upload data pipeline, use scenario name in the output path to avoid collision
    * give each submit pipeline a distinct experiment and run name for readability
    
    Co-authored-by: Jeff Omhover <jeomhove@microsoft.com>

--- examples/pipelines/pneumonia/submit.py
+++ examples/pipelines/pneumonia/submit.py
@@ -16,7 +16,6 @@ import time
 import sys
 
 # Azure ML sdk v2 imports
-import azure
 from azure.identity import DefaultAzureCredential, InteractiveBrowserCredential
 from azure.ai.ml import MLClient, Input, Output
 from azure.ai.ml.constants import AssetTypes
