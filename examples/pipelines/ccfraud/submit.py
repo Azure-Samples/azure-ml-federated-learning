3d2a039d2d46369d0fdfd0862ce80c1317093ee3
David Majercak <damajercak@microsoft.com>
2022-12-09 15:37:28 -0800

    improve plots and docs in data analytics

--- examples/pipelines/ccfraud/submit.py
+++ examples/pipelines/ccfraud/submit.py
@@ -142,6 +142,11 @@ aggregate_component = load_component(
     source=os.path.join(SHARED_COMPONENTS_FOLDER, "aggregatemodelweights", "spec.yaml")
 )
 
+if hasattr(YAML_CONFIG.federated_learning, "run_data_analysis") and YAML_CONFIG.federated_learning.run_data_analysis:
+    data_analysis_component = load_component(
+        source=os.path.join(SHARED_COMPONENTS_FOLDER, "data_analysis", "spec.yaml")
+    )
+
 
 ########################
 ### BUILD A PIPELINE ###
@@ -188,6 +193,34 @@ pipeline_identifier = getUniqueIdentifier()
     description=f'FL cross-silo basic pipeline and the unique identifier is "{pipeline_identifier}" that can help you to track files in the storage account.',
 )
 def fl_ccfraud_basic():
+    #####################
+    ### DATA-ANALYSIS ###
+    #####################
+
+    if YAML_CONFIG.federated_learning.run_data_analysis:
+        for silo_index, silo_config in enumerate(YAML_CONFIG.federated_learning.silos):
+            # run the pre-processing component once
+            silo_pre_processing_step = data_analysis_component(
+                training_data=Input(
+                    type=silo_config.training_data.type,
+                    mode=silo_config.training_data.mode,
+                    path=silo_config.training_data.path + "/train.csv",
+                ),
+                testing_data=Input(
+                    type=silo_config.testing_data.type,
+                    mode=silo_config.testing_data.mode,
+                    path=silo_config.testing_data.path + f"/test.csv",
+                ),
+                metrics_prefix=silo_config.compute,
+                **YAML_CONFIG.data_analysis_parameters
+            )
+
+            # add a readable name to the step
+            silo_pre_processing_step.name = f"silo_{silo_index}_data_analysis"
+
+            # make sure the compute corresponds to the silo
+            silo_pre_processing_step.compute = silo_config.compute
+
     ######################
     ### PRE-PROCESSING ###
     ######################
