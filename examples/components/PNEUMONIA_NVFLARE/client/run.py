06a3d1d42d98a1313ef87a5f7defd012e546fbd3
Jeff Omhover <jeomhove@microsoft.com>
2023-01-11 22:51:03 -0800

    nvflare pipeline

---
+++ examples/components/PNEUMONIA_NVFLARE/client/run.py
@@ -0,0 +1,133 @@
+import os
+import sys
+import time
+import logging
+import argparse
+import socket
+import subprocess
+import shutil
+import tempfile
+from nvflare.fuel.hci.client.fl_admin_api_runner import FLAdminAPIRunner
+from nvflare.fuel.hci.client.fl_admin_api_constants import FLDetailKey
+from nvflare.fuel.hci.client.fl_admin_api_spec import (
+    APISyntaxError,
+    FLAdminAPIResponse,
+    FLAdminAPISpec,
+    TargetType,
+)
+import mlflow
+from nvflare.security.logging import secure_format_exception
+
+
+def get_arg_parser(parser=None):
+    """Parse the command line arguments for merge using argparse.
+    Args:
+        parser (argparse.ArgumentParser or CompliantArgumentParser):
+        an argument parser instance
+    Returns:
+        ArgumentParser: the argument parser instance
+    Notes:
+        if parser is None, creates a new parser instance
+    """
+    # add arguments that are specific to the component
+    if parser is None:
+        parser = argparse.ArgumentParser(description=__doc__)
+
+    group = parser.add_argument_group("NVFlare client launcher arguments")
+    group.add_argument("--client_config", type=str, required=True)
+
+    return parser
+
+
+def run_cli_command(cli_command: list, timeout: int = None):
+    """Runs subprocess for a cli setup command"""
+    logger = logging.getLogger()
+    logger.info(f"Launching cli with command: {cli_command}")
+    cli_command_call = subprocess.run(
+        cli_command,
+        # stdout=PIPE,
+        # stderr=PIPE,
+        universal_newlines=True,
+        check=False,  # will not raise an exception if subprocess fails
+        timeout=timeout,  # TODO: more than a minute would be weird?
+        # env=custom_env
+    )
+    logger.info(f"return code: {cli_command_call.returncode}")
+
+    if cli_command_call.returncode != 0:
+        raise RuntimeError("Cli command returned code != 0")
+
+    return cli_command_call.returncode
+
+
+def run_client(client_dir):
+    """Runs the client communication process."""
+    logger = logging.getLogger()
+
+    # get Mlflow client and root run id
+    mlflow_run = mlflow.start_run()
+    mlflow_client = mlflow.tracking.client.MlflowClient()
+    logger.debug(f"parent runId: {mlflow_run.data.tags.get('mlflow.parentRunId')}")
+    root_run_id = mlflow_run.data.tags.get("mlflow.parentRunId")
+
+    overseer_name = None
+    overseer_ip = None
+    fetch_start_time = time.time()
+
+    client_dir_local = tempfile.TemporaryDirectory().name
+    shutil.copytree(client_dir, client_dir_local)
+
+    while overseer_name is None or overseer_ip is None:
+        logger.info(f"Checking out tag overseer_name/overseer_ip...")
+        mlflow_root_run = mlflow_client.get_run(root_run_id)
+
+        if "overseer_name" in mlflow_root_run.data.tags:
+            overseer_name = mlflow_root_run.data.tags["overseer_name"]
+            logger.info(f"overseer_name found: {overseer_name}")
+
+        if "overseer_ip" in mlflow_root_run.data.tags:
+            overseer_ip = mlflow_root_run.data.tags["overseer_ip"]
+            logger.info(f"overseer_ip found: {overseer_ip}")
+
+        if (overseer_name is None or overseer_ip is None) and (
+            time.time() - fetch_start_time > 600
+        ):
+            raise RuntimeError("Could not fetch the tag within timeout.")
+        else:
+            time.sleep(10)
+
+    # create hosts file to resolve ip adresses
+    with (open("/etc/hosts", "a")) as f:
+        # write server address
+        f.write(f"{overseer_ip}\t{overseer_name}\n")
+
+    # run client startup
+    startup_script_path = os.path.join(client_dir_local, "startup", "sub_start.sh")
+    logger.info(f"Running ${startup_script_path}")
+    run_cli_command(["bash", startup_script_path])
+
+
+def main():
+    """Script main function."""
+    # Create and configure logger to write into a file in job outputs/
+    logging.basicConfig(
+        filename="outputs/nvflare_host.log",
+        format="[%(asctime)s] [%(levelname)s] - %(message)s",
+        filemode="a",
+    )
+
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+
+    logger.info("sys.argv: {}".format(sys.argv))
+
+    # parse the arguments
+    parser = get_arg_parser()
+    args, _ = parser.parse_known_args()
+    logger.info("args: {}".format(args))
+
+    run_client(args.client_config)
+
+
+if __name__ == "__main__":
+    main()
---
+++ examples/pipelines/pneumonia_nvflare/submit.py
@@ -0,0 +1,291 @@
+"""NVFLARE experimental"""
+import os
+import argparse
+import random
+import string
+import datetime
+import webbrowser
+import time
+import sys
+
+# Azure ML sdk v2 imports
+import azure
+from azure.identity import DefaultAzureCredential, InteractiveBrowserCredential
+from azure.ai.ml import MLClient, Input, Output
+from azure.ai.ml.constants import AssetTypes
+from azure.ai.ml.dsl import pipeline
+from azure.ai.ml import load_component
+
+# to handle yaml config easily
+from omegaconf import OmegaConf
+
+
+############################
+### CONFIGURE THE SCRIPT ###
+############################
+
+parser = argparse.ArgumentParser(description=__doc__)
+
+parser.add_argument(
+    "--project_config",
+    type=str,
+    required=False,
+    default=os.path.join(os.path.dirname(__file__), "project.yaml"),
+    help="path to an NVFlare project.yaml file",
+)
+parser.add_argument(
+    "--nvflare_app",
+    type=str,
+    required=True,
+    help="path to an NVFlare application folder",
+)
+
+parser.add_argument(
+    "--submit",
+    default=False,
+    action="store_true",
+    help="actually submits the experiment to AzureML",
+)
+
+parser.add_argument(
+    "--subscription_id",
+    type=str,
+    required=False,
+    help="Subscription ID",
+)
+parser.add_argument(
+    "--resource_group",
+    type=str,
+    required=False,
+    help="Resource group name",
+)
+
+parser.add_argument(
+    "--workspace_name",
+    type=str,
+    required=False,
+    help="Workspace name",
+)
+
+parser.add_argument(
+    "--wait",
+    default=False,
+    action="store_true",
+    help="Wait for the pipeline to complete",
+)
+
+args = parser.parse_args()
+
+# load the config from a local yaml file
+PROJECT_CONFIG = OmegaConf.load(args.project_config)
+
+# path to the components
+COMPONENTS_FOLDER = os.path.join(
+    os.path.dirname(__file__), "..", "..", "components", "PNEUMONIA_NVFLARE"
+)
+
+
+###########################
+### CONNECT TO AZURE ML ###
+###########################
+
+
+def connect_to_aml():
+    try:
+        credential = DefaultAzureCredential()
+        # Check if given credential can get token successfully.
+        credential.get_token("https://management.azure.com/.default")
+    except Exception as ex:
+        # Fall back to InteractiveBrowserCredential in case DefaultAzureCredential does not work
+        credential = InteractiveBrowserCredential()
+
+    # Get a handle to workspace
+    try:
+        # tries to connect using cli args if provided else using config.yaml
+        ML_CLIENT = MLClient(
+            subscription_id=args.subscription_id or PROJECT_CONFIG.aml.subscription_id,
+            resource_group_name=args.resource_group
+            or PROJECT_CONFIG.aml.resource_group_name,
+            workspace_name=args.workspace_name or PROJECT_CONFIG.aml.workspace_name,
+            credential=credential,
+        )
+
+    except Exception as ex:
+        print("Could not find either cli args or config.yaml.")
+        # tries to connect using local config.json
+        ML_CLIENT = MLClient.from_config(credential=credential)
+
+    return ML_CLIENT
+
+
+####################################
+### LOAD THE PIPELINE COMPONENTS ###
+####################################
+
+# Loading the component from their yaml specifications
+provision_component = load_component(
+    source=os.path.join(COMPONENTS_FOLDER, "provision", "spec.yaml")
+)
+server_component = load_component(
+    source=os.path.join(COMPONENTS_FOLDER, "server", "spec.yaml")
+)
+client_component = load_component(
+    source=os.path.join(COMPONENTS_FOLDER, "client", "spec.yaml")
+)
+
+
+########################
+### BUILD A PIPELINE ###
+########################
+
+
+def custom_fl_data_path(
+    datastore_name, output_name, unique_id="${{name}}", iteration_num=None
+):
+    """Produces a path to store the data during FL training.
+
+    Args:
+        datastore_name (str): name of the Azure ML datastore
+        output_name (str): a name unique to this output
+        unique_id (str): a unique id for the run (default: inject run id with ${{name}})
+        iteration_num (str): an iteration number if relevant
+
+    Returns:
+        data_path (str): direct url to the data path to store the data
+    """
+    data_path = f"azureml://datastores/{datastore_name}/paths/federated_learning/{output_name}/{unique_id}/"
+    if iteration_num:
+        data_path += f"iteration_{iteration_num}/"
+
+    return data_path
+
+
+def getUniqueIdentifier(length=8):
+    """Generates a random string and concatenates it with today's date
+
+    Args:
+        length (int): length of the random string (default: 8)
+
+    """
+    str = string.ascii_lowercase
+    date = datetime.date.today().strftime("%Y_%m_%d_")
+    return date + "".join(random.choice(str) for i in range(length))
+
+
+pipeline_identifier = getUniqueIdentifier()
+
+
+@pipeline(
+    description=f'EXPERIMENTAL',
+)
+def fl_pneumonia_nvflare():
+    server_config = [ participant for participant in PROJECT_CONFIG.participants if participant.type == "server"][0]
+    admin_config = [ participant for participant in PROJECT_CONFIG.participants if participant.type == "admin"][0]
+    client_configs = [ participant for participant in PROJECT_CONFIG.participants if participant.type == "client"]
+
+    # run a provisioning component
+    provision_step = provision_component(
+        # with the config file
+        project_config=Input(
+            type=AssetTypes.URI_FILE,
+            path=args.project_config
+        )
+    )
+    provision_step.compute = server_config.azureml.compute
+    nvflare_workspace_datapath = custom_fl_data_path(
+        server_config.azureml.datastore,
+        "nvflare_workspace",
+        unique_id=pipeline_identifier
+    )
+    provision_step.outputs.workspace = Output(
+        type=AssetTypes.URI_FOLDER,
+        mode="mount",
+        path=nvflare_workspace_datapath
+    )
+    provision_step.outputs.start = Output(
+        type=AssetTypes.URI_FOLDER,
+        mode="mount",
+        path=nvflare_workspace_datapath+"/start.txt"
+    )
+
+    ## SCATTER ##
+
+    # for each silo, run a distinct client with its own inputs and outputs
+    for client_index, client_config in enumerate(client_configs):
+        silo_client_step = client_component(
+            client_config=Input(
+                type=AssetTypes.URI_FOLDER,
+                path=nvflare_workspace_datapath+f"{PROJECT_CONFIG.name}/prod_00/{client_config.name}/"
+            ),
+            start=provision_step.outputs.start
+        )
+        # add a readable name to the step
+        silo_client_step.name = f"silo_client_{client_index}"
+
+        # make sure the compute corresponds to the silo
+        silo_client_step.compute = client_config.azureml.compute
+
+    ## GATHER ##
+
+    server_step = server_component(
+        server_config=Input(
+            path=nvflare_workspace_datapath+f"{PROJECT_CONFIG.name}/prod_00/{server_config.name}/",
+            type=AssetTypes.URI_FOLDER
+        ),
+        admin_config=Input(
+            path=nvflare_workspace_datapath+f"{PROJECT_CONFIG.name}/prod_00/{admin_config.name}/",
+            type=AssetTypes.URI_FOLDER
+        ),
+        app_dir=Input(
+            type=AssetTypes.URI_FOLDER,
+            path=args.nvflare_app
+        ),
+        start=provision_step.outputs.start,
+        server_name=server_config.name,
+        expected_clients=len(client_configs)
+    )
+    # this is done in the orchestrator compute
+    server_step.compute = server_config.azureml.compute
+
+    # no return value yet
+    return {}
+
+
+pipeline_job = fl_pneumonia_nvflare()
+
+# Inspect built pipeline
+print(pipeline_job)
+
+if args.submit:
+    print("Submitting the pipeline job to your AzureML workspace...")
+    ML_CLIENT = connect_to_aml()
+    pipeline_job = ML_CLIENT.jobs.create_or_update(
+        pipeline_job, experiment_name="fl_demo_pneumonia_nvflare"
+    )
+
+    print("The url to see your live job running is returned by the sdk:")
+    print(pipeline_job.services["Studio"].endpoint)
+
+    webbrowser.open(pipeline_job.services["Studio"].endpoint)
+
+    if args.wait:
+        job_name = pipeline_job.name
+        status = pipeline_job.status
+
+        while status not in ["Failed", "Completed", "Canceled"]:
+            print(f"Job current status is {status}")
+
+            # check status after every 100 sec.
+            time.sleep(100)
+            try:
+                pipeline_job = ML_CLIENT.jobs.get(name=job_name)
+            except azure.identity._exceptions.CredentialUnavailableError as e:
+                print(f"Token expired or Credentials unavailable: {e}")
+                sys.exit(5)
+            status = pipeline_job.status
+
+        print(f"Job finished with status {status}")
+        if status in ["Failed", "Canceled"]:
+            sys.exit(1)
+else:
+    print("The pipeline was NOT submitted, use --submit to send it to AzureML.")
