69815694fb00e9d5e59dc6eba01f1c12f228d50f
ww-aims <wenwen@microsoft.com>
2023-02-08 13:59:16 -0800

    minor fix

--- examples/components/CCFRAUD/traininsilo/run.py
+++ examples/components/CCFRAUD/traininsilo/run.py
@@ -620,7 +620,7 @@ def run(args):
     )
     trainer.execute(args.checkpoint)
 
-    if torch.cuda.is_available() and int(os.environ["WORLD_SIZE"]) > 1:
+    if torch.cuda.is_available() or int(os.environ["WORLD_SIZE"]) > 1:
         dist.destroy_process_group()
 
 
