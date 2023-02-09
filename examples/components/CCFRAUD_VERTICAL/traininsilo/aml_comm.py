f9081529b81453be0583611802ca7406337382ba
David Majercak <damajercak@microsoft.com>
2023-02-09 13:36:54 -0800

    show information about closing socket connection and make it possible to run amlcommsocket offline

--- examples/components/CCFRAUD_VERTICAL/traininsilo/aml_comm.py
+++ examples/components/CCFRAUD_VERTICAL/traininsilo/aml_comm.py
@@ -107,16 +107,19 @@ class AMLCommSocket(AMLComm):
     using pickle library.
     """
 
-    def __init__(self, rank, world_size, run_id) -> None:
+    def __init__(self, rank, world_size, run_id, host_ip=None, host_port=None) -> None:
         """Initializes AMLComm communicator
 
         Args:
             rank: rank of the current node, must be between 0 and world_size-1
             world_size: number of nodes in our setup, must be positive integer larger than 1
             run_id: specifier of the run share across the nodes
+            host_ip (Optional): IP address of the host node, if not provided MLFlow is used to communicate it
+            host_port (Optional): port of the host node, if not provided MLFlow is used to communicate it
         """
         super(AMLCommSocket, self).__init__(rank, world_size, run_id)
 
+        self._host_ip, self._host_port = host_ip, host_port
         self._connections = {}
         self._setup_master()
 
@@ -129,48 +132,53 @@ class AMLCommSocket(AMLComm):
             return str(s.getsockname()[1])
 
     def _setup_master(self):
-        host_ip, host_port = None, None
+        if self._host_ip is None or self._host_port is None:
+            host_ip, host_port = None, None
+            with mlflow.start_run() as mlflow_run:
+                mlflow_client = mlflow.tracking.client.MlflowClient()
+                if self._rank == 0:
+                    host_port = self._get_open_port()
+                    local_hostname = socket.gethostname()
+                    host_ip = str(socket.gethostbyname(local_hostname))
+                    mlflow_client.set_tag(
+                        run_id=self._run_id, key="aml_host_ip", value=host_ip
+                    )
+                    mlflow_client.set_tag(
+                        run_id=self._run_id, key="aml_host_port", value=host_port
+                    )
+                else:
+                    host_ip, host_port = None, None
+                    fetch_start_time = time.time()
 
-        with mlflow.start_run() as mlflow_run:
-            mlflow_client = mlflow.tracking.client.MlflowClient()
-            if self._rank == 0:
-                host_port = self._get_open_port()
-                local_hostname = socket.gethostname()
-                host_ip = str(socket.gethostbyname(local_hostname))
-                mlflow_client.set_tag(
-                    run_id=self._run_id, key="aml_host_ip", value=host_ip
-                )
-                mlflow_client.set_tag(
-                    run_id=self._run_id, key="aml_host_port", value=host_port
-                )
-            else:
-                host_ip, host_port = None, None
-                fetch_start_time = time.time()
+                    while host_ip is None or host_port is None:
+                        logger.info(
+                            f"Checking out tag aml_host_ip and aml_host_port..."
+                        )
+                        mlflow_root_run = mlflow_client.get_run(self._run_id)
 
-                while host_ip is None or host_port is None:
-                    logger.info(f"Checking out tag aml_host_ip and aml_host_port...")
-                    mlflow_root_run = mlflow_client.get_run(self._run_id)
+                        if "aml_host_ip" in mlflow_root_run.data.tags:
+                            host_ip = mlflow_root_run.data.tags["aml_host_ip"]
+                            logger.info(f"host_ip found: {host_ip}")
 
-                    if "aml_host_ip" in mlflow_root_run.data.tags:
-                        host_ip = mlflow_root_run.data.tags["aml_host_ip"]
-                        logger.info(f"host_ip found: {host_ip}")
+                        if "aml_host_port" in mlflow_root_run.data.tags:
+                            host_port = mlflow_root_run.data.tags["aml_host_port"]
+                            logger.info(f"host_port found: {host_port}")
 
-                    if "aml_host_port" in mlflow_root_run.data.tags:
-                        host_port = mlflow_root_run.data.tags["aml_host_port"]
-                        logger.info(f"host_port found: {host_port}")
+                        if (host_ip is None) and (time.time() - fetch_start_time > 600):
+                            raise RuntimeError(
+                                "Could not fetch the tag within timeout."
+                            )
+                        else:
+                            time.sleep(10)
 
-                    if (host_ip is None) and (time.time() - fetch_start_time > 600):
-                        raise RuntimeError("Could not fetch the tag within timeout.")
-                    else:
-                        time.sleep(10)
+            self._host_ip = host_ip
+            self._host_port = host_port
 
-        self._host_ip = host_ip
-        self._host_port = host_port
         self._local_ip = str(socket.gethostbyname(socket.gethostname()))
         self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self._socket.settimeout(
-            6000
-        )  # Set timeout so we do not wait for any request indefinitely
+            600.0
+        )  # Set timeout to 30 minutes so we do not wait for any request indefinitely
 
         if self._rank == 0:
             self._socket.bind(("", int(self._host_port)))
@@ -346,6 +354,7 @@ class AMLCommSocket(AMLComm):
 
     def close(self):
         """Close the communication channels gracefully"""
+        logger.info("Closing AMLCommSocket clients")
         if self._rank == 0:
             for c in self._connections:
                 self._connections[c][0].close()
