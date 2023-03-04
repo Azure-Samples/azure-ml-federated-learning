4f0e7e2d3ab25f6f48bb7ceee9a4616e5106335e
David Majercak <damajercak@microsoft.com>
2023-03-03 19:22:21 -0800

    add bank marketing demo that holds data in the host node

---
+++ examples/components/BANK_MARKETING_VERTICAL/traininsilo/datasets.py
@@ -0,0 +1,47 @@
+import torch
+from torch.utils.data import Dataset
+
+
+class BankMarketingDataset(Dataset):
+    """SubscribeDataset Dataset - combination of features and labels
+
+    Args:
+        feature: Transaction detail tensors
+        target: Tensor of labels corresponding to features
+
+    Returns:
+        None
+    """
+
+    def __init__(self, df):
+        print(df.info())
+        if "label" in df.columns:
+            if len(df.columns) > 1:
+                self.X = torch.tensor(
+                    df.loc[:, df.columns != "label"].values, dtype=torch.float
+                )
+            else:
+                self.X = None
+            self.Y = torch.tensor(df.loc[:, "label"].values, dtype=torch.int)
+        else:
+            self.X = torch.tensor(df.values, dtype=torch.float)
+            self.Y = None
+
+    def __len__(self):
+        if self.Y is None:
+            return len(self.X)
+        else:
+            return len(self.Y)
+
+    def features_count(self):
+        if self.X is not None:
+            return self.X.shape[1]
+        return None
+
+    def __getitem__(self, idx):
+        if self.Y is None:
+            return self.X[idx]
+        elif self.X is None:
+            return self.Y[idx]
+        else:
+            return self.X[idx], self.Y[idx]
---
+++ examples/components/BANK_MARKETING_VERTICAL/traininsilo/models.py
@@ -0,0 +1,79 @@
+import torch
+import torch.nn as nn
+
+
+class SimpleLinearBottom(nn.Module):
+    """Bottom (Contributor) part of the model composed of only Linear model interleaved with ReLU activations
+
+    Args:
+        input_dim (int):
+        number of features to be consumed by the model
+    """
+
+    def __init__(self, input_dim) -> None:
+        super().__init__()
+
+        self.input_dim = input_dim
+        self.model = nn.Sequential(
+            nn.Linear(input_dim, 512),
+            nn.ReLU(),
+            nn.Linear(512, 256),
+            nn.ReLU(),
+            nn.Linear(256, 128),
+            nn.ReLU(),
+            nn.Linear(128, 64),
+            nn.ReLU(),
+            nn.Linear(64, 32),
+            nn.ReLU(),
+            nn.Linear(32, 16),
+            nn.ReLU(),
+            nn.Linear(16, 8),
+            nn.ReLU(),
+            nn.Linear(8, 4),
+            nn.ReLU(),
+        )
+        self._init_weights()
+
+    def _init_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Embedding):
+                torch.nn.init.uniform_(m.weight, -0.001, 0.001)
+            elif isinstance(m, nn.Linear):
+                torch.nn.init.xavier_uniform_(m.weight)
+                m.bias.data.fill_(0.01)
+
+    def forward(self, x):
+        return self.model(x)
+
+
+class SimpleLinearTop(nn.Module):
+    """Top (Host) part of the model composed of only Linear model interleaved with ReLU activations"""
+
+    def __init__(self, world_size) -> None:
+        super().__init__()
+
+        self._world_size = world_size
+        self.contributor_weights = torch.nn.ModuleList(
+            [nn.Linear(4, 4) for _ in range(self._world_size)]
+        )
+
+        self.model = nn.Sequential(
+            nn.Linear(4, 1),
+            nn.Sigmoid(),
+        )
+        self._init_weights()
+
+    def _init_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Embedding):
+                torch.nn.init.uniform_(m.weight, -0.001, 0.001)
+            elif isinstance(m, nn.Linear):
+                torch.nn.init.xavier_uniform_(m.weight)
+                m.bias.data.fill_(0.01)
+
+    def forward(self, x):
+        agg_x = self.contributor_weights[0](x[0])
+        for i in range(1, self._world_size - 1):
+            agg_x += self.contributor_weights[i](x[i])
+
+        return self.model(agg_x).squeeze()
