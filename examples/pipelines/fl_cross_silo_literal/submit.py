490d42535201cd48e0310ac9e5b181925fe12aea
Amit Garg <mitgarg17495@gmail.com>
2023-03-21 16:51:38 -0700

    test5

--- examples/pipelines/fl_cross_silo_literal/submit.py
+++ examples/pipelines/fl_cross_silo_literal/submit.py
@@ -24,7 +24,7 @@ from azure.ai.ml.constants import AssetTypes
 from azure.ai.ml.dsl import pipeline
 from azure.ai.ml import load_component
 
-# To Handle yaml config easily
+# To Handle YAML config easily
 from omegaconf import OmegaConf
 
 
