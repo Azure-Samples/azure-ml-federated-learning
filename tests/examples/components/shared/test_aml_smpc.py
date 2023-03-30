b8d5e0aa9484918f643b1a4439837803cc1e793d
David Majercak <damajercak@microsoft.com>
2023-03-29 21:38:21 -0700

    address comments

--- tests/examples/components/shared/test_aml_smpc.py
+++ tests/examples/components/shared/test_aml_smpc.py
@@ -11,40 +11,12 @@ from examples.components.shared.aml_smpc import AMLSMPC
 
 
 class TestAMLSMPC(unittest.TestCase):
-    def test_aml_smpc_basic(self):
-        TEST_MSG = lambda recv: f"Message to {recv}"
-
-        smpc1 = AMLSMPC()
-        smpc2 = AMLSMPC()
-
-        smpc1.add_remote_public_key(2, smpc2.get_public_key())
-        smpc2.add_remote_public_key(1, smpc1.get_public_key())
-
-        msg_2 = smpc1.encrypt(str.encode(TEST_MSG(2)), 2)
-        msg_1 = smpc2.encrypt(str.encode(TEST_MSG(1)), 1)
-
-        self.assertEquals(smpc1.decrypt(msg_1), str.encode(TEST_MSG(1)))
-        self.assertEquals(smpc2.decrypt(msg_2), str.encode(TEST_MSG(2)))
-
-    def test_aml_smpc_empty(self):
-        TEST_MSG = lambda _: ""
-
-        smpc1 = AMLSMPC()
-        smpc2 = AMLSMPC()
-
-        smpc1.add_remote_public_key(2, smpc2.get_public_key())
-        smpc2.add_remote_public_key(1, smpc1.get_public_key())
-
-        msg_2 = smpc1.encrypt(str.encode(TEST_MSG(2)), 2)
-        msg_1 = smpc2.encrypt(str.encode(TEST_MSG(1)), 1)
-
-        self.assertEquals(smpc1.decrypt(msg_1), str.encode(TEST_MSG(1)))
-        self.assertEquals(smpc2.decrypt(msg_2), str.encode(TEST_MSG(2)))
-
-    def test_aml_smpc_encodings_formats(self):
-        TEST_MSG = lambda recv: f"Message to {recv}"
-
-        for encoding, format in zip(
+    def test_aml_smpc(self):
+        message_fns = {
+            "full":lambda recv: f"Message to {recv}",
+             "empty": lambda _: f""
+        }
+        encoding_formats = zip(
             [
                 crypto_serialization.Encoding.OpenSSH,
                 crypto_serialization.Encoding.DER,
@@ -55,27 +27,22 @@ class TestAMLSMPC(unittest.TestCase):
                 crypto_serialization.PublicFormat.SubjectPublicKeyInfo,
                 crypto_serialization.PublicFormat.PKCS1,
             ],
-        ):
-            with self.subTest(encoding=encoding, format=format):
-                smpc1 = AMLSMPC()
-                smpc2 = AMLSMPC()
+        )
 
-                smpc1.add_remote_public_key(
-                    2,
-                    smpc2.get_public_key(encoding=encoding, format=format),
-                    encoding=encoding,
-                )
-                smpc2.add_remote_public_key(
-                    1,
-                    smpc1.get_public_key(encoding=encoding, format=format),
-                    encoding=encoding,
-                )
+        for message_type in ["full", "empty"]:
+            for encoding, format in encoding_formats:
+                with self.subTest(message_type=message_type, encoding=encoding, format=format):
+                    smpc1 = AMLSMPC()
+                    smpc2 = AMLSMPC()
 
-                msg_2 = smpc1.encrypt(str.encode(TEST_MSG(2)), 2)
-                msg_1 = smpc2.encrypt(str.encode(TEST_MSG(1)), 1)
+                    smpc1.add_remote_public_key(2, smpc2.get_public_key(encoding=encoding, format=format), encoding=encoding)
+                    smpc2.add_remote_public_key(1, smpc1.get_public_key(encoding=encoding, format=format), encoding=encoding)
 
-                self.assertEquals(smpc1.decrypt(msg_1), str.encode(TEST_MSG(1)))
-                self.assertEquals(smpc2.decrypt(msg_2), str.encode(TEST_MSG(2)))
+                    msg_2 = smpc1.encrypt(str.encode(message_fns[message_type](2)), 2)
+                    msg_1 = smpc2.encrypt(str.encode(message_fns[message_type](1)), 1)
+
+                    self.assertEqual(smpc1.decrypt(msg_1), str.encode(message_fns[message_type](1)))
+                    self.assertEqual(smpc2.decrypt(msg_2), str.encode(message_fns[message_type](2)))
 
     def test_aml_smpc_encoding_fail(self):
         smpc = AMLSMPC()
